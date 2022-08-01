41fb024ba87607617b409e1e10b92b90bbdcb1d7
Mark Brown <markbrownnc@gmail.com>
2022-08-01 16:10:40 -0400

    Read DVR objects back out of Atlas

--- Sources/SPMRealmCxx/main.cpp
+++ Sources/SPMRealmCxx/main.cpp
@@ -11,7 +11,7 @@
 
 struct DVRDatum: realm::object {
 
-    realm::persisted<int> _id;
+    realm::persisted<int> _id;      // not object ID yet. Could be UUID?
     realm::persisted<std::string> accounthash;
     realm::persisted<std::string> assetname;
     realm::persisted<int> bumpflags;
@@ -33,7 +33,7 @@ struct DVRDatum: realm::object {
     realm::persisted<std::string> numbertokeep;
     realm::persisted<int> parentid;
     realm::persisted<std::string> progname;
-    realm::persisted<int> programid;        // not <long>
+    realm::persisted<int> programid;        // not <long>, but we do have Int64hgy
     realm::persisted<std::string> progtmsid;
     realm::persisted<std::string> pvrorder;
     realm::persisted<std::string> rating;
@@ -116,7 +116,7 @@ DVRDatum make_sample_1() {
 
 DVRDatum make_sample_102() {
     auto dvr_device = DVRDatum();
-    dvr_device._id = 102;       // should be object ID
+    dvr_device._id = 102;       // ObjectId("62b49570996b13f09c4efe4e")
     dvr_device.accounthash = "eb9c1c569d9d32354797454e42082a4ec4eef9b16fa371f7cf0acdf4db0faeeb";
     dvr_device.dvrid = 11;
     dvr_device.devicehash = "476c6bab84ccc093ab6928e619db82b28db34c376c94d6b87dd21dd64f7f0769";
@@ -161,6 +161,57 @@ DVRDatum make_sample_102() {
     dvr_device.teamaid = "";
     dvr_device.teambid = 0;
     dvr_device.tmsserviceid = 120420;
+    
+    return dvr_device;
+}
+
+DVRDatum make_sample_103() {
+    auto dvr_device = DVRDatum();
+    dvr_device._id = 103;       // ObjectId("62b49570996b13f09c4efe50")
+    dvr_device.accounthash = "eb9c1c569d9d32354797454e42082a4ec4eef9b16fa371f7cf0acdf4db0faeeb";
+    dvr_device.dvrid = 16;
+    dvr_device.devicehash = "ab09f9063a8ddec800c37a465a19464d530cc2ed7fd53f36ab71ee846339ca6d";
+    dvr_device.assetname = "a_lqyrmT_7VHy8v";
+    dvr_device.bumpflags = 0;
+    dvr_device.category = 93;
+    dvr_device.chname = "CBS WCBS";
+    dvr_device.chnum = 502;
+    dvr_device.circuithash = "";
+    dvr_device.dtlastupdatedate = "2022-01-18 16:26:38.244+0000";
+    dvr_device.earlymins = 0;
+    dvr_device.epgduration = 3600;
+    dvr_device.epgstarttime = 1326074400;
+    dvr_device.episodename = "Who Has This One in the Bag?";
+    dvr_device.genre = "";
+    dvr_device.imgflags = 0;
+    dvr_device.lastwatched = 0;
+    dvr_device.latemins = 0;
+    dvr_device.numbertokeep = "";
+    dvr_device.parentid = 13;
+    dvr_device.progname = "The Amazing Race";
+    dvr_device.programid = 2158528683;  //should be long
+    dvr_device.progtmsid = "EP004468530472";
+    dvr_device.pvrorder = "";
+    dvr_device.rating = "";
+    dvr_device.rcrdduration = 2379;
+    dvr_device.rcrdflags = 3504;
+    dvr_device.rcrdquality = 1;
+    dvr_device.rcrdstarttime = 1326074371;
+    dvr_device.rcrdtype = 1;
+    dvr_device.resumepoint = 0;
+    dvr_device.scheduledtime = 1326066563;
+    dvr_device.seriesflags = "";
+    dvr_device.seriesid = 1979913767;
+    dvr_device.seriesname = "";
+    dvr_device.seriesrootid = 188109;
+    dvr_device.seriestmsid = "SH004468530000";
+    dvr_device.serviceid = 751;
+    dvr_device.srcclienthash = "";
+    dvr_device.state = "recorded";
+    dvr_device.status = "stopRecording";
+    dvr_device.teamaid = "";
+    dvr_device.teambid = 0;
+    dvr_device.tmsserviceid = 16689;
     
     return dvr_device;
 }
@@ -231,32 +282,43 @@ realm::task<void> run_realm() {
     devices = synced_realm.objects<DVRDatum>();
     std::cout << "devices count DEUX: " << devices.size() << std::endl;
     
-    auto dvr_device = make_sample_102();
+    auto dvr_device_102 = make_sample_102();
+    auto dvr_device_103 = make_sample_103();
 
-    synced_realm.write([&synced_realm, &dvr_device]() {
-        synced_realm.add(dvr_device);
+    synced_realm.write([&synced_realm, &dvr_device_102, &dvr_device_103]() {
+        synced_realm.add(dvr_device_102);
+        synced_realm.add(dvr_device_103);
     });
 
     co_await wait_for_sync_uploads(user);
     co_await wait_for_sync_downloads(user);
 
-    auto token = dvr_device.observe<DVRDatum>([](auto&& change) {
+    auto token = dvr_device_102.observe<DVRDatum>([](auto&& change) {
         std::cout << "property changed" << std::endl;
     });
 
-    synced_realm.write([&synced_realm, &dvr_device]() {
-        dvr_device.episodename = "episode_1";
+    synced_realm.write([&synced_realm, &dvr_device_102]() {
+        dvr_device_102.episodename = "episode_1";
     });
 
     co_await wait_for_sync_uploads(user);
     co_await wait_for_sync_downloads(user);
 
-    synced_realm.write([&synced_realm, &dvr_device]() {
-        dvr_device.episodename = "episode_2";
+    synced_realm.write([&synced_realm, &dvr_device_102]() {
+        dvr_device_102.episodename = "episode_2";
     });
 
     co_await wait_for_sync_uploads(user);
     co_await wait_for_sync_downloads(user);
+    
+    auto more_devices = synced_realm.objects<DVRDatum>();
+    std::cout << "more devices count: " << more_devices.size() << std::endl;
+    
+    std::vector<DVRDatum> myDevices;
+    std::copy(more_devices.begin(), more_devices.end(), std::back_inserter(myDevices));
+    for (auto& dev:myDevices) {
+        std::cout << "device: " << dev << std::endl;
+    }
 
     co_return;
 }
